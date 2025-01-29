506e0bd3663055d459f8db6a36c727babab33843
Jace Galloway <jace.galloway@gmail.com>
2025-01-29 13:35:28 -0500

    initial test commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import Quiz from './components/Quiz';
 
 function App() {
-  return (
-    <div className="App">
-      <Quiz />
-    </div>
-  );
+	return (
+		<div className='App'>
+			let's start quiz
+			<Quiz />
+		</div>
+	);
 }
 
 export default App;
